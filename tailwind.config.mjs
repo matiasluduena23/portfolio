d91f03ca028736ee1fcfb5de71fe462b99a32d94
matias luduena <matiasluduena23@gmail.com>
2024-12-12 18:13:04 -0300

    change content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,14 @@
-import { getParseTreeNode } from 'typescript';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				clbackground: '#252525',
-				cllightGray: '#6b6b6b',
-				clstrongGreen: '#2EB94D',
-			},
-			fontFamily: {
-				inter: ['Inter'],
-				kaisei: ['Kaisei Decol'],
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                clstrongGreen: '#41ea33',
+                clGray: '#999892',
+                clbackground: '#333',
+            },
+        },
+    },
+    plugins: [],
 };
