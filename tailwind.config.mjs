06251cfc92a63d47164de4b46cd15b669ffa127a
Matias Luduena <matiasluduena23@gmail.com>
2024-12-17 08:35:41 -0300

    font & header mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            colors: {
-                clstrongGreen: '#41ea33',
-                clGray: '#999892',
-                clbackground: '#333',
-            },
-        },
-    },
-    plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				clstrongGreen: "#41ea33",
+				clGray: "#999892",
+				clbackground: "#333",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+				basker: ["Libre Baskerville", "serif"],
+			},
+		},
+	},
+	plugins: [],
 };
